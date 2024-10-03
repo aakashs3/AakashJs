21e00c7c862a4d25d618b8802656ddb31bb17033
aakashs3 <129573799+aakashs3@users.noreply.github.com>
2024-10-03 06:16:09 +0000

    Variables

---
+++ part01/variables.js
@@ -0,0 +1,11 @@
+const accountId = 123456
+let accountName = "Aakash"
+var accountPassword = "7999"
+
+/* declare variables agr change karna ho to niche jesa karo */
+
+accountName="Solanki"
+accountPassword = "123"
+console.log(accountId);
+console.table({accountId,accountName,accountPassword})
+
